1611806402d511e6d327130655c3ef994bdad364
Andrey Kuzmenko <andrey.k.qwerty@gmail.com>
2019-07-06 21:11:49 +0300

    task full - done

---
+++ src/test/java/ru/javawebinar/topjava/service/JdbcMealServiceTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.ActiveDbProfileResolver;
+
+@ActiveProfiles({"jdbc"})
+public class JdbcMealServiceTest extends MealServiceTest {
+}
